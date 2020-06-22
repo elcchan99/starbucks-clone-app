f25c4f284bfaa4c94ecdfed396e07ea9dfb4aeca
edgar.chan <edgar.chan@cloverhealth.com>
2020-06-22 09:14:00 +0800

    upate starbuck green rgb code

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const STARBUCKS_GREEN = Color.fromRGBO(75, 165, 105, 1);
+const STARBUCKS_GREEN = Color.fromRGBO(0, 157, 91, 1);
 const PARE_GREY = Color.fromRGBO(108, 108, 108, 1);
